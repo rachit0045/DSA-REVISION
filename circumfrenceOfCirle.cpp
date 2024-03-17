60b2dc65761979aee729e5e4ae936c5694a11bdd
rachit0045 <rachitsharma9022@gmail.com>
2024-03-17 10:34:10 +0530

    print circumfrence of circle

---
+++ circumfrenceOfCirle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float r;
+    cout << "enter the radius of circle :";
+    cin >> r;
+    float coc = 2 * 3.14 * r;
+    cout << "circumfrence of circle is :" << coc << endl;
+    return 0;
+}
