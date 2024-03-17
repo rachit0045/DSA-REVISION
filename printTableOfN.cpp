17949a993f1dcc09042d4c49d7e692746a50fc52
rachit0045 <rachitsharma9022@gmail.com>
2024-03-17 11:37:00 +0530

    print the table of n

---
+++ printTableOfN.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter the table of :";
+    cin >> n;
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i * n << endl;
+    }
+    return 0;
+}
