cc9d16862a94c16780db2a882765a81ab76057a8
rachit0045 <rachitsharma9022@gmail.com>
2024-03-17 10:27:18 +0530

    print the size of data types

---
+++ sizeOfDataType.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "size of int data type is :" << sizeof(int) << "bytes" << endl;
+    cout << "size of bool data type is :" << sizeof(bool) << "bytes" << endl;
+    cout << "size of float data type is :" << sizeof(float) << "bytes" << endl;
+
+    return 0;
+}
